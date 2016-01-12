19f00ebc8e0fba0aa97222710704836e9b3aa6f8
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2016-01-11 22:38:57 -0300

    added the principal features of the program and the delete by description feature

--- todolist.rb
+++ todolist.rb
@@ -1,8 +1,8 @@
 class TodoList
 
-	attr_reader :title, :items
+	attr_accessor :title, :items
 
-
+    # Method that create the list
     def initialize(list_title)
     	@title=list_title;
     	@items= Array.new;
@@ -13,17 +13,61 @@ class TodoList
         item = Item.new(new_item)
         @items.push(item)
     end
+
+    # Deletes an Item from the array of Items by sending the description
+    def delete_item_by_pos(position)
+    	#since array are zero based datastructures we rest 1 to the position
+    	position -=1
+    	@items.delete_at(position);
+    end
+
+    # Deletes an Item from the array of Items by sending the description
+    def delete_item_by_description(item_description)
+
+    	@items.each do |item|
+	    	if item.item_description_equals?(item_description)
+    	    	@items.delete(item)
+       			puts "item deleted"
+	    	end
+	    end
+    end
+
+    def rename_list(name)
+    	@title=name
+    end
+
+
+    def print_list 
+
+    	puts "-------------"
+    	puts @title
+    	puts "-------------"
+    	counter = 0;
+    	@items.each do |item|
+    		puts counter.to_s + " " + item.description + " completed " + item.completed_status.to_s;
+    		counter +=1;
+    	end
+    end
 end
 
 class Item
     # methods and stuff go here
-  	attr_reader :description, :completed_status
+  	attr_accessor :description, :completed_status
 
     def initialize(item_description)
-    	@description=description;
-    	@id=id;
+    	@description=item_description;
     	@completed_status = false
     end
 
+
+    def item_description_equals?(description)
+	  	if @description.eql?(description);
+	  		return true;
+	  	end
+  	end
+
+  	def update_status(status)
+  		@status = status
+  	end
 
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,18 +1,27 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+todo = TodoList.new("TodoList")
 
 # Add four new items
+todo.add_item("Play Piano")
+todo.add_item("Study Math")
+todo.add_item("Run a Mile")
+todo.add_item("Go to the Market")
 
 # Print the list
-
+todo.print_list
 # Delete the first item
+todo.delete_item_by_pos(1)
 
 # Print the list
+todo.print_list
 
 # Delete the second item
+todo.delete_item_by_pos(2)
 
 # Print the list
+todo.print_list
 
 # Update the completion status of the first item to complete
 
@@ -21,3 +30,5 @@ require_relative 'todolist.rb'
 # Update the title of the list
 
 # Print the list
+
+
